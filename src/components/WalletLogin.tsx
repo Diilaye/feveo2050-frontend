1b7430313828db04aac67fd2f4d965718da22ab8
Diilaye <issa.k@saharux.com>
2025-08-20 16:00:15 +0000

    add news version

--- src/components/WalletLogin.tsx
+++ src/components/WalletLogin.tsx
@@ -37,7 +37,7 @@ const WalletLogin: React.FC = () => {
 
       if (data.success) {
         // Vérifier si le statut d'enregistrement n'est pas validé
-        if (data.data.gieInfo && data.data.gieInfo.statutEnregistrement !== 'validee') {
+        if (data.data.gieInfo && data.data.gieInfo.statutEnregistrement !== 'valide') {
           console.log('GIE non validé, création d\'une transaction d\'adhésion');
           
           try {
